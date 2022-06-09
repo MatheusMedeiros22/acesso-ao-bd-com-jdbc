276ea1b76985f36a4c0cb6dbce01e2a0d00bf6bb
Matheus Medeiros <mathmed22@gmail.com>
2022-06-09 18:49:18 -0300

    Recuperar dados

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,12 +4,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
 
 	private static Connection conn = null;
+	
+	
 
 	public static Connection getConnection() {
 		if (conn == null) {
@@ -42,6 +46,26 @@ public class DB {
 			return props;
 		} catch (IOException e) {
 			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
